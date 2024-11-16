314c80ab47915754306f7e8dbcd418b0c64dd317
MarkoIvanetic <-C>
2024-11-16 18:57:42 +0100

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const path = require("path");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname), // Adds support for '@/...' aliases
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
