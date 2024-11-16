2ea85efd268674cc8cdfbb334cf1ea525e4d44a1
MarkoIvanetic <-C>
2024-11-16 21:53:38 +0100

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import EventList from "@/components/features/EventList";
-import React from "react";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Events</h1>
-      <EventList />
+      <Suspense>
+        <EventList />
+      </Suspense>
     </div>
   );
 }
